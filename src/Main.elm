a2a28dbe7c766ede8f465e504e64e081cd8c963c
selaere <12009104+selaere@users.noreply.github.com>
2023-05-10 23:32:12 +0200

    color :)

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,9 @@ import Html exposing (Html, text, pre)
 import Http
 import Dict
 import Dict exposing (Dict)
+import Html exposing (Attribute)
+import Html.Attributes
+import Murmur3 exposing (hashString)
 
 main : Program () Model Msg
 main = Browser.element {
@@ -57,30 +60,36 @@ view model = case model of
     Failure -> text "loading failed epically"
     Loading -> text "loading..."
     Success fullText -> let scores = parse fullText in Html.div [] [
-        make_table scores,
+        make_table scores{-,
         pre [] [ scores
             |> Dict.values
             |> List.map (\{score, mode, date, owner} -> 
                 owner ++ " " ++ (String.fromInt score) ++ " in " ++ mode ++ " at " ++ date)
             |> String.join "\n"
-            |> text ]]
+            |> text ]-}]
+
+player_color : String -> Attribute msg
+player_color name =
+    let hash = hashString 3054 name in
+    let hue = hash |> modBy 360 |> String.fromInt in
+    let lgt = ((hash // 360) |> modBy 45) + 40 |> String.fromInt in
+    Html.Attributes.style "background-color" ("hsl("++hue++",60%,"++lgt++"%")
 
 make_cell : Dict String Score -> String -> Html msg
 make_cell scores mode = case Dict.get mode scores of
-    Just score -> Html.td [] [
-        text (String.fromInt score.score),
-        Html.small [] [" " ++ score.mode |> text],
+    Just {score, owner} -> Html.td [player_color owner] [
+        text (String.fromInt score),
+        Html.small [] [text (" " ++ mode)],
         Html.br [] [],
-        Html.small [] [score.owner |> text] ]
+        Html.small [] [text owner] ]
     Nothing -> Html.td [] [text "so sad"]
 
 make_table : Dict String Score -> Html msg
 make_table scores =
     let modesstr = modes |> List.map String.fromChar in
-    (modesstr)
-    |> List.indexedMap Tuple.pair
+    modesstr |> List.indexedMap Tuple.pair
     |> List.map (\(x, m1) ->
-        modesstr 
+        (""::modesstr)
         |> List.take (x+1)
         |> List.map (\m2 -> make_cell scores (if m1 /= m2 then m2 ++ m1 else m1))
         |> Html.tr [])
