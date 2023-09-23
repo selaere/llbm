8036ba0a7aeeba9852f7c0315198eaa754c87356
selaere <12009104+selaere@users.noreply.github.com>
2023-09-23 13:08:21 +0200

    whoops date dont work

--- src/JsStuff.js
+++ src/JsStuff.js
@@ -1,4 +1,4 @@
 const {v3} =require("../../murmurhash/murmurhash.js");
-const formatSecs = x=>new Date(x).toISOString();
-const showSecs   = x=>formatSecs(x).slice(0,19).replace("T"," ");
+const formatSecs = x=>new Date(x).toISOString().slice(0,19);
+const showSecs   = x=>formatSecs(x).replace("T"," ");
 export{v3,formatSecs,showSecs}
